1dace080949a6a6659e6616a1172bef6f4d97e5c
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2020-08-09 12:05:45 -0300

    Arrow function e seu escopo léxico

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,25 +1,21 @@
 class ListaNegociacoes {
 
-    constructor(contexto, armadilha) {
+    constructor(armadilha) {
+
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
     }
-
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
-    
+
     get negociacoes() {
         return [].concat(this._negociacoes);
     }
 
     esvazia() {
         this._negociacoes = [];
-        //this._armadilha(this);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
-
 }
